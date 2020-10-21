9b53ac0060ab34ad6bb83d03035c73d9a116a844
hannah-slalom <hannah.ingham@slalom.com>
2020-10-21 07:45:32 -0700

    removing unused resources and grouping all twilio resources

--- create-flow-translations.js
+++ create-flow-translations.js
@@ -7,11 +7,6 @@ const dictionary = process.argv[3];
 const filePrefix = process.argv[4];
 const description = process.argv[5];
 
-
-console.log("Parent Template: ", parentFlowPath, dictionary, "\n");
-console.log("Dictionary: ", dictionary, "\n");
-
-
 const templateFlow = new TwilioFlowTranslation(parentFlowPath, ["EN", "ES-US"]);
 
 templateFlow.generateFlows(dictionary, description, filePrefix);
